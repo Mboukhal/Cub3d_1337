fab12c7cf0ef019afb6e6f966c40ce47e214b0a1
Mohammed Boukhal <mboukhal@e1r1p5.1337.ma>
2022-09-01 14:29:29 +0100

    Thu Sep  1 14:27:24 +01 2022

--- src/checks.c
+++ src/checks.c
@@ -6,7 +6,7 @@
 /*   By: mboukhal <mboukhal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/24 08:18:43 by mboukhal          #+#    #+#             */
-/*   Updated: 2022/08/24 11:40:48 by mboukhal         ###   ########.fr       */
+/*   Updated: 2022/09/01 14:29:27 by mboukhal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ void	check_exteniton(char *file)
 }
 
 /*
-	load file to cub structure
+	TODO:load file to cub structure
 	
 	check map skip space and check for '1' or '\n' without skipping
 */
