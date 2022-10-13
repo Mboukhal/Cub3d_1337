8b64e6b97494f576cade719f0175be5c9904f4e9
mboukhal <lios80466@gmail.com>
2022-10-13 08:35:12 +0100

    Thu Oct 13 08:35:06 +01 2022

--- src/parsing/affiche_cub.c
+++ src/parsing/affiche_cub.c
@@ -6,29 +6,29 @@
 /*   By: mboukhal <mboukhal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/02 22:11:45 by ahmaidi           #+#    #+#             */
-/*   Updated: 2022/10/12 18:46:33 by mboukhal         ###   ########.fr       */
+/*   Updated: 2022/10/13 08:07:49 by mboukhal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cub.h"
 
-void	affiche_cub(t_cub *cub)
-{
-	int	i;
-	int	j;
+// void	affiche_cub(t_cub *cub)
+// {
+// 	int	i;
+// 	int	j;
 
-	i = 0;
-	printf("======= MAP========\n");
-	if (cub->size_map == 0)
-		return ;
-	while (cub->map[i])
-	{
-		j = 0;
-		while(cub->map[i][j])
-		{
-			printf("%c ==> (%d, %d)\n", cub->map[i][j], i , j);
-			j++;
-		}
-		i++;
-	}
-}
+// 	i = 0;
+// 	printf("======= MAP========\n");
+// 	if (cub->size_map == 0)
+// 		return ;
+// 	while (cub->map[i])
+// 	{
+// 		j = 0;
+// 		while(cub->map[i][j])
+// 		{
+// 			printf("%c ==> (%d, %d)\n", cub->map[i][j], i , j);
+// 			j++;
+// 		}
+// 		i++;
+// 	}
+// }
